6b175bc3073ba04d9007ecd758219172a6a3a1c8
NisriinaWakhdah <ninaputri3344@gmail.com>
2025-10-22 15:57:14 +0700

    Implement CRUD

--- nina_media_gallery/urls.py
+++ nina_media_gallery/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 app_name = 'nina_media_gallery'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.gallery_list, name='gallery_list'),
+    path('upload-media/', views.gallery_upload, name='gallery_upload'),
+    path('media/<uuid:id>/update', views.gallery_update, name='gallery_update'),
+    path('media/<uuid:id>/delete', views.gallery_delete, name='gallery_delete'),
+]
