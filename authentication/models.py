2eccf1ed607cc3deb25568d5f7ffb36ece8067e7
NisriinaWakhdah <ninaputri3344@gmail.com>
2025-10-23 07:50:08 +0700

    Add some changes

--- authentication/models.py
+++ authentication/models.py
@@ -1,1 +1,11 @@
+from django.db import models
 from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    alamat = models.TextField()
+    umur = models.PositiveIntegerField()
+    nomor_handphone = models.CharField(max_length=20)
+
+    def str(self):
+        return self.user.username
