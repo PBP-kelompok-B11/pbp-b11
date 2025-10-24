a46476d53ce472bd22d21597487864b99ca067f7
sheriqa <andisheriqadewina@gmail.com>
2025-10-25 00:21:35 +0700

    csrf

--- beyond90/settings.py
+++ beyond90/settings.py
@@ -30,6 +30,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "a-sheriqa-beyond-90.pbp.cs.ui.ac.id",]
+#tes
 CSRF_TRUSTED_ORIGINS = [
     "https://a-sheriqa-beyond-90.pbp.cs.ui.ac.id/",
 ]
