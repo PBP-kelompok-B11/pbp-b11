40ba74f6454907075d181419dc37ae02dc3312e5
sheriqa <andisheriqadewina@gmail.com>
2025-10-25 23:53:18 +0700

    smoga bisa

--- beyond90/settings.py
+++ beyond90/settings.py
@@ -146,7 +146,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR / 'static']
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
