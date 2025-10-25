848441ffe2ea2b4d7fb7e0cd2707cb1f50716e02
sheriqa <andisheriqadewina@gmail.com>
2025-10-25 16:21:09 +0700

    Update ALLOWED_HOSTS, CSRF_TRUSTED_ORIGINS for deploy

--- beyond90/settings.py
+++ beyond90/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = 'django-insecure-z%xc3%tvv@ylnf5rfw=!s426oop%&4euso*i0nfi%8@-*tb^-)
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "a-sheriqa-beyond-90.pbp.cs.ui.ac.id",]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "a-sheriqa-beyond-90.pbp.cs.ui.ac.id"]
 #tes
 CSRF_TRUSTED_ORIGINS = [
-    "https://a-sheriqa-beyond-90.pbp.cs.ui.ac.id/",
+    "https://a-sheriqa-beyond-90.pbp.cs.ui.ac.id",
 ]
 # Application definition
 
