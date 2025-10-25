5fdd642499da45ca9fb7e71f504111316e831238
vidiaqonita <qonitavidia@gmail.com>
2025-10-26 00:00:09 +0700

    coba ya

--- beyond90/settings.py
+++ beyond90/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-z%xc3%tvv@ylnf5rfw=!s426oop%&4euso*i0nfi%8@-*tb^-)
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
-
+#nambah perubahan
 ALLOWED_HOSTS = [ "a-sheriqa-beyond-90.pbp.cs.ui.ac.id", "https://a-sheriqa-beyond-90.pbp.cs.ui.ac.id", "http://a-sheriqa-beyond-90.pbp.cs.ui.ac.id", "localhost", "127.0.0.1"]
 #tes sesuatu
 CSRF_TRUSTED_ORIGINS = [
