58a54630a4733693116c439ee1dd2224844e7624
sheriqa <andisheriqadewina@gmail.com>
2025-11-26 16:25:38 +0700

    perbarui search dan bikin endpoint api

--- search/urls.py
+++ search/urls.py
@@ -1,4 +1,3 @@
-# search/urls.py
 from django.urls import path
 from . import views
 
@@ -11,4 +10,12 @@ urlpatterns = [
     path('players/', views.search_players, name='search_players'),
     path('clubs/', views.search_clubs, name='search_clubs'),
     path('events/', views.search_events, name='search_events'),
+
+    # ======================
+    # API FLUTTER
+    # ======================
+    path('api/search/', views.api_search, name='api_search'),
+    path('api/search/history/', views.api_history, name='api_search_history'),
+    path('api/search/history/clear/', views.api_history_clear, name='api_history_clear'),
+    path('api/search/history/<int:history_id>/', views.api_history_delete_item, name='api_history_delete_item'),
 ]
