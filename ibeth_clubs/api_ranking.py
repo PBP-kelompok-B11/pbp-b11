73a8283fbca6aee443fb3357f261bcf9e65e36f2
sheriqa <andisheriqadewina@gmail.com>
2025-12-21 13:05:56 +0700

    coba

--- ibeth_clubs/api_ranking.py
+++ ibeth_clubs/api_ranking.py
@@ -16,16 +16,6 @@ class RankingListAPI(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def put(self, request, pk):
-        ranking = self.get_object(pk)
-        if ranking is None:
-            return Response({"error": "Ranking not found."}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = ClubRankingSerializer(ranking, data=request.data)  
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -38,3 +28,14 @@ class RankingDetailAPI(APIView):
 
         serializer = ClubRankingSerializer(ranking)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+            ranking = self.get_object(pk)
+            if ranking is None:
+                return Response({"error": "Ranking not found."}, status=status.HTTP_404_NOT_FOUND)
+
+            serializer = ClubRankingSerializer(ranking, data=request.data)  
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
