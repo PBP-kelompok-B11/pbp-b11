71be4ad0bcf7d25ea499fee8c45ed1ed3ada86e4
sheriqa <andisheriqadewina@gmail.com>
2025-12-21 11:47:22 +0700

    fixing fitur ibeth

--- ibeth_clubs/api_ranking.py
+++ ibeth_clubs/api_ranking.py
@@ -10,6 +10,13 @@ class RankingListAPI(APIView):
         serializer = ClubRankingSerializer(rankings, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = ClubRankingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RankingDetailAPI(APIView):
     def get(self, request, pk):
